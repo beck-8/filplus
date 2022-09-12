4c000e1e2acfd02210f61b03df4d3b081dd772cd
beck <beck@beckdeMacBook-Pro.local>
2022-09-13 00:14:19 +0800

    release v0.1.0

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 var CurrentCommit string
 
 // BuildVersion is the local build version
-const BuildVersion = "0.0.1"
+const BuildVersion = "0.1.0"
 
 func UserVersion() string {
 	return BuildVersion + "+git." + CurrentCommit
