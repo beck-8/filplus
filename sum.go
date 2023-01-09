7e0d5c272c75c5e8eb384f048dfff1dadc3b85a9
beck <1504068285@qq.com>
2023-01-09 22:35:52 +0800

    add calculate datacap

---
+++ sum.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/filecoin-project/go-state-types/builtin/v9/market"
+	"github.com/urfave/cli/v2"
+	"io/ioutil"
+	"os"
+	"strings"
+	"text/tabwriter"
+	"time"
+)
+
+type MarketDeal struct {
+	Proposal market.DealProposal
+	State    market.DealState
+}
+
+type Deal struct {
+	JsonRpc string                 `json:"jsonrpc"`
+	Result  map[string]*MarketDeal `json:"result"`
+	Id      int                    `json:"id"`
+}
+
+var sum = &cli.Command{
+	Name:  "calculate",
+	Usage: "from file calculate datacap",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:     "sp",
+			Value:    "",
+			Usage:    "specify sp list",
+			Required: false,
+			Aliases:  []string{"s"},
+		},
+		&cli.StringFlag{
+			Name:     "client",
+			Value:    "",
+			Usage:    "specify client id list",
+			Required: false,
+			Aliases:  []string{"c"},
+		},
+		&cli.StringFlag{
+			Name:     "file",
+			Value:    "",
+			Usage:    "specify deal file",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:  "start",
+			Value: "2020-08-25 06:00:00",
+			Usage: "specify start time",
+		},
+		&cli.StringFlag{
+			Name:  "end",
+			Value: "2060-08-25 06:00:00",
+			Usage: "specify end time",
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		var startEpoch, endEpoch int64
+		var err error
+
+		if start := ctx.String("start"); start != "" {
+			startEpoch, err = timeToHeight(start)
+			if err != nil {
+				return err
+			}
+		}
+		if end := ctx.String("end"); end != "" {
+			endEpoch, err = timeToHeight(end)
+			if err != nil {
+				return err
+			}
+		}
+
+		file := ctx.String("file")
+		deal := Deal{}
+		f, err := ioutil.ReadFile(file)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(f, &deal)
+		if err != nil {
+			return err
+		}
+
+		sp_deal := map[string]map[string]int64{}
+		fmt.Printf("%s ~ %s\n", ctx.String("start"), ctx.String("end"))
+
+		w := tabwriter.NewWriter(os.Stdout, 18, 0, 4, ' ',
+			0)
+		fmt.Fprint(w, "client\tsp\tdatacap(T)\n")
+		var totalDc int64
+
+		for _, v := range deal.Result {
+
+			provider := v.Proposal.Provider.String()
+			client := v.Proposal.Client.String()
+			pieceSize := v.Proposal.PieceSize
+			sectorStartEpoch := int64(v.State.SectorStartEpoch)
+
+			//if !v.Proposal.VerifiedDeal && sectorStartEpoch != -1 {
+			if sectorStartEpoch != 1 {
+				if strings.Contains(ctx.String("sp"), provider) && strings.Contains(ctx.String("client"), client) {
+					if sectorStartEpoch >= startEpoch && sectorStartEpoch <= endEpoch {
+						if _, ok := sp_deal[provider]; ok {
+							sp_deal[provider][client] += int64(pieceSize)
+						} else {
+							sp_deal[provider] = map[string]int64{}
+							sp_deal[provider][client] += int64(pieceSize)
+						}
+						totalDc += int64(pieceSize)
+					}
+
+				}
+			}
+
+		}
+
+		for sp, v := range sp_deal {
+			for client, piecesize := range v {
+				fmt.Fprintf(w, "%s\t%s\t%v\n", client, sp, piecesize/(1<<40))
+			}
+		}
+
+		fmt.Fprintf(w, "Total Datacap\t\t%v\n", totalDc/(1<<40))
+		w.Flush()
+		return nil
+
+	},
+}
+
+func timeToHeight(text string) (int64, error) {
+	// 主网启动时间
+	bootstrapTime := int64(1598306400)
+	// 中国时区
+	loc, _ := time.LoadLocation("PRC")
+	stamp, err := time.ParseInLocation("2006-1-2 15:04:05", text, loc)
+	if err != nil {
+		return 0, err
+	}
+	return (stamp.Unix() - bootstrapTime) / 30, nil
+}
